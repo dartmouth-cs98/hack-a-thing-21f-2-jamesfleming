0cd46962a0d8d12a031d32d354fb05c846310033
James  Fleming <jamesfleming@Jamess-MacBook-Pro.local>
2021-09-29 14:26:54 -0400

    Working with some models, strange model bugs

--- HackAThing2/HackAThing2/ViewController.swift
+++ HackAThing2/HackAThing2/ViewController.swift
@@ -19,6 +19,9 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
+        // implement the delegate so that did add anchors gets called
+        arView.session.delegate = self
+        
         // Setup AR view (in reality kit this is automatically done, but in advanced AR applications likely want to override this
         
         setupARView()
@@ -52,17 +55,87 @@ class ViewController: UIViewController {
     @objc
     func handleTap(recognizer: UITapGestureRecognizer){
         // Translate tap into coordinate in space
-        let location = recognizer.location(in:arView)
+        let tapLocation = recognizer.location(in:arView)
         
         // Use a raycast to determine if this location is intersecting with a real world physical surface
         
         // We are use plane detection to find these surfaces
+        // set allowing to either be more or less precise about what planes are allowed
+        // Then we set to only find horizontal planes
+        let results = arView.raycast(from: tapLocation , allowing: .estimatedPlane, alignment: .horizontal)
         
+    
+        // Check if we have a result
+        if let firstResult = results.first{
+            // All objects must be added to anchors
+            // To create multi user AR experiences we synchronize to these anchors
+            
+            let anchor = ARAnchor(name:"shoe",transform: firstResult.worldTransform)
+            
+            arView.session.add(anchor: anchor)
+            
+            print("shoed added")
+        }else{
+            
+            let verticalResults = arView.raycast(from: tapLocation, allowing: .estimatedPlane, alignment: .vertical)
+            
+            if let verticalResult = verticalResults.first{
+                let anchor = ARAnchor(name:"clock", transform: verticalResult.worldTransform)
+                arView.session.add(anchor: anchor)
+                print("clock added")
+            }else{
+                
+                print("No Surface found")
+            }
+            
+        }
         
-        
+       
     }
     
+    func placeObject(named entityName: String, for anchor: ARAnchor){
+        // All objects in reality scene are entities, in this case model entity
+        
+        
+        // Create a model entity
+        
+        guard let path = Bundle.main.path(forResource: entityName, ofType: "usdz")else{
+            fatalError("Couldn't find file")
+        }
+        let url = URL(fileURLWithPath: path)
+       
+        
+        let entity = try! ModelEntity.loadModel(contentsOf: url, withName:"BearStatue")
+        
+        
+        // Allow us to drag and rotate object
+        entity.generateCollisionShapes(recursive: true)
+        arView.installGestures([.rotation, .translation, .scale], for: entity)
+        
+        
+
+        // Create anchor entity
+        let anchorEntity = AnchorEntity(anchor: anchor)
+        anchorEntity.addChild(entity)
+        
+        // Add it to our scene
+        arView.scene.addAnchor(anchorEntity)
+        
+    }
+
+
+}
+
+// Author of tutorial notes that there are easier ways to just place an object using RealityKit, but we are using ARkit to allow for future more complicated effects
+extension ViewController : ARSessionDelegate{
+    func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+        // Go through the anchors
+        for anchor in anchors{
+            if let anchorName = anchor.name, (anchorName == "shoe" || anchorName == "clock"){
+                placeObject(named:anchorName, for: anchor)
+                
+            }
+        }
+    }
     
-    
-   
 }
